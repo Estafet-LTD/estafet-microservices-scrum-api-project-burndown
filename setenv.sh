0c86f926d83494eb870db91fc7442c19882e28c6
dennis williams <dennis.williams@estafet.com>
2017-09-17 22:30:29 +0100

    Added broker credentials

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,7 @@
 export JBOSS_A_MQ_BROKER_URL=tcp://localhost:61616
+export JBOSS_A_MQ_BROKER_USER=estafet
+export JBOSS_A_MQ_BROKER_PASSWORD=estafet
+
 export PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://localhost:5432/project-burndown
 export PROJECT_BURNDOWN_REPOSITORY_DB_USER=postgres
 export PROJECT_BURNDOWN_REPOSITORY_DB_PASSWORD=welcome1
