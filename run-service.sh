37abab32a6300392054d71b25f8853deded788d3
dennis williams <dennis.williams@estafet.com>
2017-10-12 19:47:33 +0100

    Added instrumentation for jms and jdbc for opentracing.

--- run-service.sh
+++ run-service.sh
@@ -1,1 +1,1 @@
-java -jar target/estafet-microservices-scrum-api-project-burndown-*.jar  --server.port=9001
+java -jar target/estafet-microservices-scrum-api-project-burndown-*.jar  --server.port=9001 --debug
