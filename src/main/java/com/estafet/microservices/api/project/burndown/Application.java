10052c9efb586fbbda9e40b7e886dc3cf1686cc1
dennis williams <dennis.williams@estafet.com>
2017-10-12 21:26:34 +0100

    Additional instrumentation for listeners.

--- src/main/java/com/estafet/microservices/api/project/burndown/Application.java
+++ src/main/java/com/estafet/microservices/api/project/burndown/Application.java
@@ -14,11 +14,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.SimpleMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import io.opentracing.Tracer;
+import io.opentracing.contrib.jms.spring.TracingJmsTemplate;
 import io.opentracing.contrib.jms.spring.TracingMessageConverter;
 
 @Configuration
@@ -49,6 +51,13 @@ public class Application extends SpringBootServletInitializer {
 		return restTemplateBuilder.build();
 	}
 
+	@Bean
+	public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory, Tracer tracer) {
+	   JmsTemplate jmsTemplate = new TracingJmsTemplate(connectionFactory, tracer);
+	   jmsTemplate.setMessageConverter(new SimpleMessageConverter());
+	   return jmsTemplate;
+	}
+	
 	@Bean
 	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
 			DefaultJmsListenerContainerFactoryConfigurer configurer) {
