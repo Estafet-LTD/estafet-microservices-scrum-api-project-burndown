26bfa06a6d473922af433c9c7b65fad3312bdd3f
dennis williams <dennis.williams@estafet.com>
2017-10-27 21:01:55 +0100

    Now listens for new sprints.

---
+++ src/main/java/com/estafet/microservices/api/project/burndown/jms/NewSprintConsumer.java
@@ -0,0 +1,32 @@
+package com.estafet.microservices.api.project.burndown.jms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import com.estafet.microservices.api.project.burndown.model.ProjectBurndownSprint;
+import com.estafet.microservices.api.project.burndown.service.ProjectBurndownService;
+
+import io.opentracing.ActiveSpan;
+import io.opentracing.Tracer;
+
+@Component
+public class NewSprintConsumer {
+
+	@Autowired
+	private Tracer tracer;
+
+	@Autowired
+	private ProjectBurndownService projectBurndownService;
+
+	@JmsListener(destination = "new.sprint.topic", containerFactory = "myFactory")
+	public void onMessage(String message) {
+		ActiveSpan span = tracer.activeSpan().log(message);
+		try {
+			projectBurndownService.updateBurndown(ProjectBurndownSprint.fromJSON(message));	
+		} finally {
+			span.close();
+		}
+	}
+
+}
