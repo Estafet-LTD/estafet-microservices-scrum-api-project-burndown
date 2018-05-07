4dccdb893f9689ca94639ba6104f7309810885fd
Dennis Williams <dennis.williams@estafet.com>
2018-05-07 21:54:50 +0100

    modified tests for rest

--- src/integration-test/java/com/estafet/microservices/api/project/burndown/container/tests/ITProjectBurndownTest.java
+++ src/integration-test/java/com/estafet/microservices/api/project/burndown/container/tests/ITProjectBurndownTest.java
@@ -2,12 +2,9 @@ package com.estafet.microservices.api.project.burndown.container.tests;
 
 import static org.junit.Assert.*;
 
-import java.net.HttpURLConnection;
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -17,23 +14,10 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import javax.jms.Topic;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -50,11 +34,10 @@ public class ITProjectBurndownTest {
 		get("/api").then().body("id", equalTo(1)).body("title", equalTo("my project"));
 	}
 
-	@Ignore
 	@Test
 	@DatabaseSetup("ITProjectBurndownTest-data.xml")
 	public void testGetProjectBurndown() {
-		fail("Not yet implemented");
+		get("/project/1/burndown").then().body("id", is("1")).body("title", is("My Project #6889"));
 	}
 	
 	@Ignore
