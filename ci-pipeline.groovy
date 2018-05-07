23696fd546d21e4a8c1bc8cb15c5d5d661219fb3
Dennis Williams <dennis.williams@estafet.com>
2018-05-07 23:28:15 +0100

    alwasy generate integration test results.

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -25,23 +25,26 @@ node("maven") {
 	}
 
 	stage("build & deploy container") {
-		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "3000000"
+		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "300000"
 		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
 	}
 
 	stage("container tests") {
-		withEnv(
-			[	"PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
-				"PROJECT_BURNDOWN_REPOSITORY_DB_USER=postgres", 
-				"PROJECT_BURNDOWN_REPOSITORY_DB_PASSWORD=welcome1",
-				"PROJECT_BURNDOWN_SERVICE_URI=http://${microservice}.${project}-scrum.svc:8080",
-				"JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616",
-				"JBOSS_A_MQ_BROKER_USER=amq",
-				"JBOSS_A_MQ_BROKER_PASSWORD=amq"
-			]) {
-				sh "mvn verify -P integration-test"
+		try {
+			withEnv(
+				[	"PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+					"PROJECT_BURNDOWN_REPOSITORY_DB_USER=postgres", 
+					"PROJECT_BURNDOWN_REPOSITORY_DB_PASSWORD=welcome1",
+					"PROJECT_BURNDOWN_SERVICE_URI=http://${microservice}.${project}-scrum.svc:8080",
+					"JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616",
+					"JBOSS_A_MQ_BROKER_USER=amq",
+					"JBOSS_A_MQ_BROKER_PASSWORD=amq"
+				]) {
+					sh "mvn verify -P integration-test"
+				}
+			} finally {
+				junit "**/target/failsafe-reports/*.xml"
 			}
-			junit "**/target/failsafe-reports/*.xml"
 	}
 	
 	stage("tag container for testing") {
