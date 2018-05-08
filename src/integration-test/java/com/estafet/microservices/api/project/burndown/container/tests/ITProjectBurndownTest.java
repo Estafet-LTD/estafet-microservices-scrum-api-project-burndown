e2051217e977553e00430f387a63c96a414fac35
Dennis Williams <dennis.williams@estafet.com>
2018-05-08 15:01:51 +0100

    added ids to sprint and project

--- src/integration-test/java/com/estafet/microservices/api/project/burndown/container/tests/ITProjectBurndownTest.java
+++ src/integration-test/java/com/estafet/microservices/api/project/burndown/container/tests/ITProjectBurndownTest.java
@@ -70,8 +70,8 @@ public class ITProjectBurndownTest {
 	@Test
 	@DatabaseSetup("ITProjectBurndownTest-empty.xml")
 	public void testNewProject() throws Exception {
-		projectTopic.send("{\"title\":\"My Project #1\",\"noSprints\":5,\"sprintLengthDays\":5}");
-		sprintTopic.send("{ \\\"id\\\": 1, \\\"startDate\\\": \\\"2017-10-01 00:00:00\\\", \\\"endDate\\\": \\\"2017-10-06 00:00:00\\\", \\\"number\\\": 1, \\\"status\\\": \\\"Completed\\\",  \\\"projectId\\\": 1,  \\\"noDays\\\": 5 }");
+		projectTopic.send("{ \"id\": 1, \"title\":\"My Project #1\",\"noSprints\":5,\"sprintLengthDays\":5}");
+		sprintTopic.send("{ \"id\": 1, \"startDate\": \"2017-10-01 00:00:00\", \"endDate\": \"2017-10-06 00:00:00\", \"number\": 1, \"status\": \"Completed\",  \"projectId\": 1,  \"noDays\": 5 }");
 		Thread.sleep(2000);
 		get("/project/1/burndown").then()
 			.body("id", is(1))
