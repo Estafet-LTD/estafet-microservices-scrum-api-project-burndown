f9d4bc3accfe98838c0d1f4d45b736a4ef48ce91
Dennis Williams <dennis.williams@estafet.com>
2018-05-09 09:54:38 +0100

    test to see if the sprint is not sent

--- src/integration-test/java/com/estafet/microservices/api/project/burndown/container/tests/ITProjectBurndownTest.java
+++ src/integration-test/java/com/estafet/microservices/api/project/burndown/container/tests/ITProjectBurndownTest.java
@@ -66,8 +66,8 @@ public class ITProjectBurndownTest {
 	public void testNewProject() throws Exception {
 		projectTopic.send("{ \"id\": 1, \"title\":\"My Project #1\",\"noSprints\":5,\"sprintLengthDays\":5}");
 		Thread.sleep(2000);
-		sprintTopic.send("{ \"id\": 1, \"startDate\": \"2017-10-01 00:00:00\", \"endDate\": \"2017-10-06 00:00:00\", \"number\": 1, \"status\": \"Active\",  \"projectId\": 1,  \"noDays\": 5 }");
-		Thread.sleep(2000);
+		//sprintTopic.send("{ \"id\": 1, \"startDate\": \"2017-10-01 00:00:00\", \"endDate\": \"2017-10-06 00:00:00\", \"number\": 1, \"status\": \"Active\",  \"projectId\": 1,  \"noDays\": 5 }");
+		//Thread.sleep(2000);
 		get("/project/1/burndown").then()
 			.body("id", is(1))
 			.body("title", is("My Project #1"))
