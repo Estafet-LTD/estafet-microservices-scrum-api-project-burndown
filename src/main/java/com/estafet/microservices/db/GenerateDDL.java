b4b4401b7a590b8f5494d42875ab7299b8a6ace5
Dennis Williams <dennis.williams@estafet.com>
2018-05-11 14:41:29 +0100

    updated ddl generation so that it required almost no modification for each microservice and can be copied as is.

--- src/main/java/com/estafet/microservices/db/GenerateDDL.java
+++ src/main/java/com/estafet/microservices/db/GenerateDDL.java
@@ -1,4 +1,4 @@
-package com.estafet.microservices.api.project.burndown.db;
+package com.estafet.microservices.db;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,19 +9,39 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class GenerateDDL {
 
 	public static void main(String[] args) throws IOException {
-		String microservice = args[0];
+		String microservice = getMicroserviceName();
 		File create = new File("target", "create-" + microservice + "-db.ddl");
 		File drop = new File("target", "drop-" + microservice + "-db.ddl");
 		create.delete();
 		drop.delete();
-		new ClassPathXmlApplicationContext(microservice + "-ddl-application-context.xml").close();
+		new ClassPathXmlApplicationContext("generate-ddl-application-context.xml").close();
 		appendSemicolon(create);
 		appendSemicolon(drop);
+	}
+
+	private static String getMicroserviceName() {
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder;
+			dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(new File("pom.xml"));
+			doc.getDocumentElement().normalize();
+			return doc.getElementsByTagName("microservice").item(0).getTextContent();
+		} catch (DOMException | ParserConfigurationException | SAXException | IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private static void appendSemicolon(File ddl) throws IOException {
