d7bf8b286d62c91c344061b3fc951648859d9284
Dennis Williams <dennis.williams@estafet.com>
2018-05-16 13:40:29 +0100

    tests should be more reliable now

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -34,28 +34,30 @@ node("maven") {
 	}
 
 	stage("build & deploy container") {
-		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "300000", env : [ [ name : "JBOSS_A_MQ_BROKER_URL", value : "tcp://broker-amq-tcp.${project}.svc:61616" ] ]
-		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000" 
+		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "300000"
+		sh "oc set env dc/${microservice} JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616 -n ${project}"
+		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
+		sleep time:90 
 	}
 
 	stage("container tests") {
-		try {
-			withEnv(
-				[	"PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
-					"PROJECT_BURNDOWN_REPOSITORY_DB_USER=postgres", 
-					"PROJECT_BURNDOWN_REPOSITORY_DB_PASSWORD=welcome1",
-					"PROJECT_BURNDOWN_SERVICE_URI=http://${microservice}.${project}.svc:8080",
-					"JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616",
-					"JBOSS_A_MQ_BROKER_USER=amq",
-					"JBOSS_A_MQ_BROKER_PASSWORD=amq"
-				]) {
-				withMaven(mavenSettingsConfig: 'microservices-scrum') {
+		withEnv(
+			[	"PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+				"PROJECT_BURNDOWN_REPOSITORY_DB_USER=postgres", 
+				"PROJECT_BURNDOWN_REPOSITORY_DB_PASSWORD=welcome1",
+				"PROJECT_BURNDOWN_SERVICE_URI=http://${microservice}.${project}.svc:8080",
+				"JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616",
+				"JBOSS_A_MQ_BROKER_USER=amq",
+				"JBOSS_A_MQ_BROKER_PASSWORD=amq"
+			]) {
+			withMaven(mavenSettingsConfig: 'microservices-scrum') {
+				try {
 					sh "mvn clean verify -P integration-test"
+				} finally {
+					sh "oc set env dc/${microservice} JBOSS_A_MQ_BROKER_URL=tcp://localhost:61616 -n ${project}"
 				}
-			}			
-		} finally {
-			sh "oc set env dc/${microservice} JBOSS_A_MQ_BROKER_URL=tcp://localhost:61616 -n ${project}"
-		}
+			}
+		}			
 	}
 
 	stage("deploy snapshots") {
