d3c3f87fb974a57fbbfc1a7ac9d6b461cf95a3ac
Dennis Williams <dennis.williams@estafet.com>
2018-06-01 19:38:37 +0100

    only perform release after qa tests have been run successfully

--- pipelines/release-pipeline.groovy
+++ pipelines/release-pipeline.groovy
@@ -33,29 +33,6 @@ node('maven') {
 	stage("checkout") {
 		git branch: "master", url: "https://${username()}:${password()}@github.com/Estafet-LTD/estafet-microservices-scrum-api-project-burndown"
 	}
-	
-	stage("increment version") {
-		def pom = readFile('pom.xml');
-		def matcher = new XmlSlurper().parseText(pom).version =~ /(\d+\.\d+\.)(\d+)(\-SNAPSHOT)/
-		developmentVersion = "${matcher[0][1]}${matcher[0][2].toInteger()+1}-SNAPSHOT"
-		releaseVersion = "${matcher[0][1]}${matcher[0][2]}"
-		releaseTag = "v${releaseVersion}"
-	}
-	
-	stage("perform release") {
-        sh "git config --global user.email \"jenkins@estafet.com\""
-        sh "git config --global user.name \"jenkins\""
-        withMaven(mavenSettingsConfig: 'microservices-scrum') {
-			sh "mvn release:clean release:prepare release:perform -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize -B"
-			sh "git push origin master"
-			sh "git tag ${releaseTag}"
-			sh "git push origin ${releaseTag}"
-		} 
-	}	
-
-	stage("tag image") {
-		openshiftTag namespace: project, srcStream: microservice, srcTag: 'PrepareForTesting', destinationNamespace: 'prod', destinationStream: microservice, destinationTag: releaseVersion
-	}
 
 	stage("update database") {
 		sh "oc get pods --selector app=postgresql -o json -n ${project} > pods.json"
@@ -91,6 +68,29 @@ node('maven') {
 	stage("trigger acceptance tests") {
 		sh "oc start-build qa-pipeline -n cicd"	
 	}
+	
+	stage("increment version") {
+		def pom = readFile('pom.xml');
+		def matcher = new XmlSlurper().parseText(pom).version =~ /(\d+\.\d+\.)(\d+)(\-SNAPSHOT)/
+		developmentVersion = "${matcher[0][1]}${matcher[0][2].toInteger()+1}-SNAPSHOT"
+		releaseVersion = "${matcher[0][1]}${matcher[0][2]}"
+		releaseTag = "v${releaseVersion}"
+	}
+	
+	stage("perform release") {
+        sh "git config --global user.email \"jenkins@estafet.com\""
+        sh "git config --global user.name \"jenkins\""
+        withMaven(mavenSettingsConfig: 'microservices-scrum') {
+			sh "mvn release:clean release:prepare release:perform -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize -B"
+			sh "git push origin master"
+			sh "git tag ${releaseTag}"
+			sh "git push origin ${releaseTag}"
+		} 
+	}	
+
+	stage("tag image") {
+		openshiftTag namespace: project, srcStream: microservice, srcTag: 'PrepareForTesting', destinationNamespace: 'prod', destinationStream: microservice, destinationTag: releaseVersion
+	}	
 	
 }
 
