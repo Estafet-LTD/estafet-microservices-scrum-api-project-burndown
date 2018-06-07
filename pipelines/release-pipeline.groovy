0346e1134db9e4e65ca04ed1dd5a259f305ec62e
Dennis Williams <dennyjoewilliams@yahoo.com>
2018-06-07 22:59:11 +0100

    fixed create db

--- pipelines/release-pipeline.groovy
+++ pipelines/release-pipeline.groovy
@@ -38,7 +38,7 @@ node('maven') {
 		sh "oc get pods --selector app=postgresql -o json -n ${project} > pods.json"
 		def json = readFile('pods.json')
 		def pod = new groovy.json.JsonSlurper().parseText(json).items[0].metadata.name	
-		def template = readFile ('createdb.sh.template').replaceAll(/\$\{microservice\}/, microservice)
+		def template = readFile ('ddl/createdb.sh.template').replaceAll(/\$\{microservice\}/, microservice)
 		writeFile file:"createdb.sh", text:template
 		sh "oc rsync --no-perms=true --include=\"*.ddl\" --exclude=\"*\" ddl/ ${pod}:/tmp -n ${project}"	
 		sh "oc rsync --no-perms=true --include=\"createdb.sh\" --exclude=\"*\" ddl/ ${pod}:/tmp -n ${project}"	
