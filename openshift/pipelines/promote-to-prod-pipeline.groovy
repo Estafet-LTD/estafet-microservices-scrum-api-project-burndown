7998fb6f8bd7dc8cf79b01c9d394d658df330f64
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-06 21:53:22 +0100

    fixed creation of production database

--- openshift/pipelines/promote-to-prod-pipeline.groovy
+++ openshift/pipelines/promote-to-prod-pipeline.groovy
@@ -100,13 +100,13 @@ node("maven") {
 	
 	stage("ensure the exists database") {
 		withMaven(mavenSettingsConfig: 'microservices-scrum') {
-	      sh "mvn clean package -P create-db -Dmaven.test.skip=true -Dproject=${project}"
+	      sh "mvn clean package -P create-prod-db -Dmaven.test.skip=true -Dproject=${project}"
 	    } 
 	}		
 	
 	stage("create deployment config") {
 		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} -p DOCKER_NAMESPACE=staging -p DOCKER_IMAGE_LABEL=${version} | oc apply -f -"
-		sh "oc set env dc/${microservice} JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"	
+		sh "oc set env dc/${microservice} PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/prod-${microservice} JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"	
 	}
 	
 	stage("execute deployment") {
