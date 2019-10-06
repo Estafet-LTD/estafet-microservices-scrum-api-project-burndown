fe4b0f8bdb38e5f192ab7a0746486905496117bb
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-06 18:29:31 +0100

    fixed the additional environment variable parameters and also check for non existent pods

--- openshift/pipelines/promote-to-prod-pipeline.groovy
+++ openshift/pipelines/promote-to-prod-pipeline.groovy
@@ -55,11 +55,17 @@ boolean isLatestVersionDeployed(project, microservice, version) {
 	def image = readFile('image.json')
 	def imageStreamHash = getImageStreamHash(image, version)
 	println "image stream hash $imageStreamHash"
-	sh "oc get pods --selector deploymentconfig=${microservice} -n ${project} -o json > pod.json"
-	def pod = readFile('pod.json')
-	def podImageHash = getPodImageHash(pod)
-	println "pod image hash $podImageHash"
-	return imageStreamHash.equals(podImageHash)
+	sh "oc get pods --selector deploymentconfig=${microservice} -n ${project} > exists.json"
+	def exists =  = readFile('exists.json')
+	if (!exists.indexOf("No resources")>= 0) {
+		sh "oc get pods --selector deploymentconfig=${microservice} -n ${project} -o json > pod.json"
+		def pod = readFile('pod.json')
+		def podImageHash = getPodImageHash(pod)
+		println "pod image hash $podImageHash"
+		return imageStreamHash.equals(podImageHash)	
+	} else {
+		return false
+	}
 }
 
 node("maven") {
@@ -100,7 +106,7 @@ node("maven") {
 	
 	stage("create deployment config") {
 		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} -p DOCKER_NAMESPACE=staging -p DOCKER_IMAGE_LABEL=${version} | oc apply -f -"
-		sh "oc set env dc/${microservice} SPRINT_API_SERVICE_URI=http://sprint-api.${project}.svc:8080 STORY_API_SERVICE_URI=http://story-api.${project}.svc:8080 TASK_API_SERVICE_URI=http://task-api.${project}.svc:8080 PROJECT_API_SERVICE_URI=http://project-api.${project}.svc:8080 SPRINT_BOARD_API_SERVICE_URI=http://sprint-board.${project}.svc:8080 SPRINT_BURNDOWN_SERVICE_URI=http://sprint-burndown.${project}.svc:8080 PROJECT_BURNDOWN_SERVICE_URI=http://project-burndown.${project}.svc:8080 JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"	
+		sh "oc set env dc/${microservice} JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"	
 	}
 	
 	stage("execute deployment") {
